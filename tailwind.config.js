3218195e996e11a0ad2b7eff80c885b6d3f9c7b4
Robin Verton <hello@robinverton.de>
2022-02-19 21:56:38 +0100

    Revert "add breakpoint for wide screen"
    This reverts commit ac6a1697f1e1d1ccb99813f2fcd72a434e5c800d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
     screens: {
-      desktop: '850px',
-      wide: '1200px'
+      desktop: '850px'
     },
     colors: {
       white: '#FFFFFF',
