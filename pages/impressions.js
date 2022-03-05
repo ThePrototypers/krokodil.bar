aae79651c7491b9c8bf55747778020dab1c0fbb8
Eike Caldeweyher <hello@eikecaldeweyher.de>
2022-03-05 18:58:06 +0100

    Add two pictures to impressions.

--- pages/impressions.js
+++ pages/impressions.js
@@ -6,13 +6,17 @@ export default function Impressions() {
     <Layout>
       <Headline title="Impressionen" />
       <div className="flex flex-col mt-8 p-2 desktop:p-0 desktop:flex-row desktop:mt-12">
-        <div className="w-full desktop:w-1/2">
+        <div className="w-full desktop:w-1/3">
           <div className="desktop:p-2 mb-5"><img src="/img/gallery/Kroko_2.jpg" /></div>
           <div className="desktop:p-2 mb-5"><img src="/img/gallery/Kroko_4.jpg" /></div>
         </div>
-        <div className="w-full desktop:w-1/2">
+        <div className="w-full desktop:w-1/3">
           <div className="desktop:p-2 mb-5"><img src="/img/gallery/Kroko_3.jpg" /></div>
-          <div className="desktop:p-2"><img src="/img/gallery/Kroko_5.jpeg" /></div>
+          <div className="desktop:p-2 mb-5"><img src="/img/gallery/Kroko_5.jpeg" /></div>
+        </div>
+        <div className="w-full desktop:w-1/3">
+          <div className="desktop:p-2 mb-5"><img src="/img/gallery/Kroko_Bar1.jpg" /></div>
+          <div className="desktop:p-2"><img src="/img/gallery/Kroko_Raucherraum1.jpg" /></div>
         </div>
       </div>
     </Layout >
