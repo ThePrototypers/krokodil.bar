263ad0f44d1c2d5968f9880fb955e6de36614d7a
Eike Caldeweyher <hello@eikecaldeweyher.de>
2022-03-08 16:39:49 +0100

    Set margin botton on date.

--- pages/news.js
+++ pages/news.js
@@ -1,8 +1,8 @@
 import { importAllPosts } from "utils/news";
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
 
-import Layout from 'pages/components/layout.js'
-import Headline from 'pages/components/headline'
+import Layout from "pages/components/layout.js";
+import Headline from "pages/components/headline";
 
 export async function getStaticProps() {
   // Get posts for the first page
@@ -10,7 +10,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts
+      posts,
     },
   };
 }
@@ -18,33 +18,35 @@ export async function getStaticProps() {
 export default function News({ posts }) {
   return (
     <Layout>
-
       <Headline title="Aktuelles" />
 
-      <div className="space-y-20 mt-8 mb-10 desktop:mt-14">
-        {
-          posts.map((post, idx) => {
-            return (
-              <div
-                key={idx}
-                className="bg-white py-11 px-2 desktop:p-16 desktop:rounded-3xl bg-repeat"
-                style={{ backgroundImage: "url('/img/tile.png')", backgroundSize: "25%" }}
-              >
-                <div>
-                  <h5 className="text-krokodil-yellow-dark font-yanone font-semibold text-base2 desktop:text-2xl">{
-                    format(parseISO(post.attributes.date), 'd.MM.y')
-                  }</h5>
-                  <h3 className="uppercase s3 text-krokodil-yellow-dark desktop:mt-[6px]">{post.attributes.title}</h3>
+      <div className="space-y-20 mt-8 mb-12 desktop:mt-14">
+        {posts.map((post, idx) => {
+          return (
+            <div
+              key={idx}
+              className="bg-white py-11 px-2 desktop:p-16 desktop:rounded-3xl bg-repeat"
+              style={{
+                backgroundImage: "url('/img/tile.png')",
+                backgroundSize: "25%",
+              }}
+            >
+              <div>
+                <h5 className="text-krokodil-yellow-dark mb-4 font-yanone font-semibold text-base2 desktop:text-2xl">
+                  {format(parseISO(post.attributes.date), "d.MM.y")}
+                </h5>
+                <h3 className="uppercase s3 text-krokodil-yellow-dark desktop:mt-[6px]">
+                  {post.attributes.title}
+                </h3>
 
-                  <div className="prose-xl">
-                    <div dangerouslySetInnerHTML={{ __html: post.html }} />
-                  </div>
+                <div className="prose-xl">
+                  <div dangerouslySetInnerHTML={{ __html: post.html }} />
                 </div>
               </div>
-            )
-          })
-        }
+            </div>
+          );
+        })}
       </div>
-    </Layout >
-  )
+    </Layout>
+  );
 }
